c7480d2b6638d7958a8353462bc1604c5b600572
nataliaSumali <natalia.sumali@gmail.com>
2015-01-13 12:10:59 -0800

    first commit, authentication

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Journal24wr::Application.config.session_store :cookie_store, key: '_journal24wr_session'
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Journal24wr::Application.routes.draw do
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :sessions
+
+  resources :users
+
+  root 'users#index'
+
+end
